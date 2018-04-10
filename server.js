d454412c9dfef0419283b3984095665c3277301a
suuuth <suther98@msu.edu>
2018-04-10 15:49:50 -0400

    fixing var port

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var server = http.createServer(function (request, response) {
 
 // Listen on port 8080, so that we can reach the app at
 // localhost:8080
-var port = 8080
+var port = process.env.PORT || 8080
 server.listen(port)
 
 // Output a friendly message to the terminal
